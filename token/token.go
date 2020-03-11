ba9e07b8401aee796fba3e1989ac2c7786f5977e
tainakag <tainakg@yahoo-corp.jp>
2020-03-12 02:10:43 +0900

    lex letter and number

--- token/token.go
+++ token/token.go
@@ -33,3 +33,15 @@ const (
     LET = "LET"
 )
 
+var keywords = map[string]TokenType{
+    "fn": FUNCTION,
+    "let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+    if tok, ok := keywords[ident]; ok {
+        return tok
+    }
+    return IDENT
+}
+
