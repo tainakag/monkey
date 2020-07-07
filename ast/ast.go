115a207e75e6f6e826d95479a323e1b223e6dc7c
tainakag <t.nkawa.83@gmail.com>
2020-07-08 06:31:55 +0900

    add return and expression statemens. And add String() function to Node interface

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,13 @@
 package ast
 
-import "github.com/tainakag/monkey/token"
+import (
+    "github.com/tainakag/monkey/token"
+    "bytes"
+)
 
 type Node interface {
 	TokenLiteral() string
+    String() string
 }
 
 type Statement interface {
@@ -16,6 +20,7 @@ type Expression interface {
 	expressionNode()
 }
 
+// Program
 type Program struct {
 	Statements []Statement
 }
@@ -28,6 +33,15 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+    var out bytes.Buffer
+    for _, s := range p.Statements {
+        out.WriteString(s.String())
+    }
+    return out.String()
+}
+
+// LetStatement
 type LetStatement struct {
 	Token token.Token // token.LET トークン
 	Name  *Identifier
@@ -38,7 +52,22 @@ func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
+func(ls *LetStatement) String() string {
+    var out bytes.Buffer
+    out.WriteString(ls.TokenLiteral() + " ")
+    out.WriteString(ls.Name.String())
+    out.WriteString(" = ")
 
+    if ls.Value != nil {
+        out.WriteString(ls.Value.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
+// Identifier
 type Identifier struct {
 	Token token.Token // token.IDENT トークン
 	Value string
@@ -47,4 +76,50 @@ type Identifier struct {
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
+}
+
+func (i *Identifier) String() string {
+    return i.Value
+}
+
+// ReturnStatement
+type ReturnStatement struct {
+    Token token.Token  // 'return'トークン
+    ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+    return rs.Token.Literal
+}
+
+func(rs *ReturnStatement) String() string {
+    var out bytes.Buffer
+    out.WriteString(rs.TokenLiteral() + " ")
+
+    if rs.ReturnValue != nil {
+        out.WriteString(rs.ReturnValue.String())
+    }
+
+    out.WriteString(";")
+
+    return out.String()
+}
+
+// ExpressionStatement
+type ExpressionStatement struct {
+    Token token.Token  // 式の最初のトークン
+    Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode() {}
+func (es *ExpressionStatement) TokenLiteral() string {
+    return es.Token.Literal
+}
+
+func(es *ExpressionStatement) String() string {
+    if es.Expression != nil {
+        return es.Expression.String()
+    }
+    return ""
 }
--- parser/parser.go
+++ parser/parser.go
@@ -43,6 +43,8 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
     case token.LET:
         return p.parseLetStatement()
+    case token.RETURN:
+        return p.parseReturnStatement()
     default:
         return nil
     }
@@ -65,6 +67,18 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
     for !p.curTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
+    return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+    stmt := &ast.ReturnStatement{Token: p.curToken}
+    p.nextToken()
+
+    // TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+    for !p.curTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
     return stmt
 }
 
