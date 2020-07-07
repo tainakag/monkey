ae42173d9638efa77f1db41ebe06d90b8cf2e2ee
tainakag <t.nkawa.83@gmail.com>
2020-07-07 18:04:56 +0900

    add peekErrors for debug in parser

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"github.com/tainakag/monkey/ast"
 	"github.com/tainakag/monkey/lexer"
 	"github.com/tainakag/monkey/token"
+    "fmt"
 )
 
 type Parser struct {
@@ -11,16 +12,26 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+    errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+    p := &Parser{l: l, errors: []string{},}
 
 	// 2つのトークンを読み込む。curTokenとpeekToken の両方がセットされる。
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -70,6 +81,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
 }
